e96a96f244e7500084b2806edf96cce3c03f9ec0
gtrancourt <gtrancourt@gmail.com>
2019-08-27 22:25:01 +0200

    Performance metrics of images works in command line mode (loop over dir)

--- nb_slices_evaluation/Nb_slices_performance_metrics.py
+++ nb_slices_evaluation/Nb_slices_performance_metrics.py
@@ -8,9 +8,11 @@ Created on 2019-08-20
 """
 
 import os
+import joblib
 import numpy as np
 import pandas as pd
 import skimage.io as io
+import sys
 from tabulate import tabulate
 
 def performance_metrics(stack,gp_test_slices,label_stack,label_test_slices,folder_name,sample_name):
@@ -19,10 +21,10 @@ def performance_metrics(stack,gp_test_slices,label_stack,label_test_slices,folde
     # performance_metrics(processed,gridphase_test_slices_subset,label_stack,label_test_slices_subset,folder_name,tag)
     # generate absolute confusion matrix
     conf_matrix = pd.crosstab(stack[gp_test_slices,:,:].ravel(order="F"),label_stack[label_test_slices,:,].ravel(order="F"),rownames=['Actual'], colnames=['Predicted'])
-    conf_matrix.to_csv(folder_name+'Results/'+sample_name+'_ConfusionMatrix.txt',header='Predicted', index='Actual', sep='\t', mode='w')
+    conf_matrix.to_csv(folder_name+sample_name+'_ConfusionMatrix.txt',header='Predicted', index='Actual', sep='\t', mode='w')
     # generate normalized confusion matrix
     conf_matrix_norm = pd.crosstab(stack[gp_test_slices,:,:].ravel(order="F"),label_stack[label_test_slices,:,].ravel(order="F"), rownames=['Actual'], colnames=['Predicted'], normalize='index')
-    conf_matrix_norm.to_csv(folder_name+'Results/'+sample_name+'_NormalizedConfusionMatrix.txt',header='Predicted', index='Actual', sep='\t', mode='w')
+    conf_matrix_norm.to_csv(folder_name+sample_name+'_NormalizedConfusionMatrix.txt',header='Predicted', index='Actual', sep='\t', mode='w')
     print(tabulate(conf_matrix_norm, headers='keys', tablefmt='pqsl'))
     # total acccuracy
     total_testpixels = stack.shape[1]*stack.shape[2]*len(gp_test_slices)
@@ -39,9 +41,9 @@ def performance_metrics(stack,gp_test_slices,label_stack,label_test_slices,folde
                 'recall': recall,
                 'f1_score': f1_score}
     results_out = pd.DataFrame(data_out)
-    results_out.to_csv(folder_name+'Results/'+sample_name+'_PerformanceMetrics.txt', sep='\t', encoding='utf-8')
+    results_out.to_csv(folder_name+sample_name+'_PerformanceMetrics.txt', sep='\t', encoding='utf-8')
 
-    file = open(folder_name+'Results/'+sample_name+'_PerformanceMetrics.txt', 'a')
+    file = open(folder_name+sample_name+'_PerformanceMetrics.txt', 'a')
     file.write("\n")
     file.write("\nTotal accuracy: "+str(total_accuracy*100)+"%\n")
     file.write("Total test pixels: "+str(total_testpixels))
@@ -57,30 +59,45 @@ def print_feature_layers(rf_t,folder_name):
         file.write('Feature_layer {fl} importance: {imp}'.format(fl=fl, imp=imp)+'\n')
     file.close()
 
+# GET THE DATA FROM THE COMMAND LINE INPUT
+full_script_path = sys.argv[0]
+filename = sys.argv[1]
+labelled_name = sys.argv[2]
+raw_slices = sys.argv[3]
+path = os.getcwd()
+file_w_path = os.path.realpath(filename)
+filesize = os.path.getsize(file_w_path)
+
+# STOP IF THIS IS A FULL STACK PREDICTION
+# WILL DEAL WITH THAT LATER
+if filesize > 1e8:
+    print('************************************************')
+    raise ValueError('Not a predicted stack on labelled slices only -- Skipping')
+
 # DEFINE SOME NAMES
-folder_name = '/Users/guillaumetherouxrancourt/Dropbox/PostDoc/Projects/LeafTrait_MachineLearning/Test_Data_for_Manuscript/'
-filename = 'C_I_12_Strip2_fullstack_prediction_train_2_test_8.tif'
+folder_name = path + '/Results/'
 name_split = filename[:-4].split('_') # Remove file extension and split
 sample_name = '_'.join(name_split[0:4])
 test_name = '_'.join(name_split[0:4] + name_split[6:10])
 train_slices = list(map(int, name_split[7].split('-')))
 test_slices = list(map(int, name_split[9].split('-')))
+model_filename = '_'.join(name_split[0:4]) + '_RF_model_' + '_'.join(name_split[6:10]) + '.joblib'
 
 # CHECK IF RESULTS FOLDER EXISTS
-if os.path.exists(folder_name + 'Results/') == False:
-    os.makedirs(folder_name + 'Results/')
+if os.path.exists(folder_name) == False:
+    os.makedirs(folder_name)
 
 # LOAD HAND LABELLED STACK
-labelled_stack = io.imread(folder_name + 'labelled-stack.tif_2x-smaller.tif')
+labelled_stack = io.imread(path + '/' + labelled_name)
 print(np.unique(labelled_stack))
 cell_lbl, epid_lbl, bs_lbl, vein_lbl, bg_lbl, air_lbl = np.unique(labelled_stack)
 
 # LABELLED SLICES ON ORIGINAL STACK
-labelled_slices = np.array([80,140,200,260,340,400,440,540,620,740,800,860,940,1060,1140,1240,1300,1400,1480,1540,1600,1690,1740,1840])-1
+labelled_slices = np.array([int(x) for x in raw_slices.split(',')]) - 1
 labelled_slices_seq = np.arange(len(labelled_slices))
 
 # LOAD PREDICTED STACK
-pred_stack = io.imread(folder_name + 'NB_slices_eval/C_I_12_Strip2_fullstack_prediction_train_2_test_8.tif')
+pred_stack = io.imread(file_w_path)
 print(np.unique(pred_stack))
 if np.any(np.unique(pred_stack) < 0):
     pred_stack = np.where(pred_stack < 0, pred_stack + 256, pred_stack)
@@ -98,17 +115,15 @@ pred_stack = np.where(pred_stack == bg_pred, bg_lbl, pred_stack)
 indices = [x for x in labelled_slices_seq if x != train_slices]
 conf_matrix, precision, recall = performance_metrics(pred_stack,indices,labelled_stack,indices,folder_name,test_name)
 
-# LOAD MODEL
-if os.path.isfile(folder_name+sample_name+'RF_model.joblib'):
-    print('***LOADING TRAINED MODEL***')
-    rf_transverse = joblib.load(folder_name+sample_name+'RF_model.joblib')
-    print(('Our Out Of Box prediction of accuracy is: {oob}%'.format(
-        oob=rf_transverse.oob_score_ * 100)))
-
-
-    if pred_size == 'label':
-        gp_test_slices = label_test_slices
-    elif pred_size == 'min-max':
-
-# OOB predictions
-print('Our Out Of Box prediction of accuracy is: {oob}%'.format(oob=rf_transverse.oob_score_ * 100))
+# # LOAD MODEL
+# rf_transverse = joblib.load(folder_name + model_filename)
+#     print(('Our Out Of Box prediction of accuracy is: {oob}%'.format(
+#         oob=rf_transverse.oob_score_ * 100)))
+#
+#
+#     if pred_size == 'label':
+#         gp_test_slices = label_test_slices
+#     elif pred_size == 'min-max':
+#
+# # OOB predictions
+# print('Our Out Of Box prediction of accuracy is: {oob}%'.format(oob=rf_transverse.oob_score_ * 100))
