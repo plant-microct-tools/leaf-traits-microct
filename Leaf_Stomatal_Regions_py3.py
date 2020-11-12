e48bb58fb339f7be3033c9206bc1de3b4ca85ab7
gtrancourt <gtrancourt@gmail.com>
2020-11-12 12:18:48 -0500

    Defining filepath when extracting data out of sample_path_split

--- Leaf_Stomatal_Regions_py3.py
+++ Leaf_Stomatal_Regions_py3.py
@@ -202,16 +202,18 @@ sample_path_split = path_to_sample.split('/')
 if len(sample_path_split) == 1:
     sample_name = path_to_sample
     filename = sample_name + 'SEGMENTED.tif'
+    filepath = base_folder_name + sample_name + '/'
 elif len(sample_path_split) == 2:
     sample_name = sample_path_split[-2]
     filename = sample_path_split[-1]
+    filepath = base_folder_name + sample_name + '/'
 elif len(sample_path_split) == 3:
     sample_name = sample_path_split[-3]
     base_folder_name = base_path + '/' + sample_name + '/' + sample_path_split[-2] + '/'
+    filepath = base_folder_name
     filename = sample_path_split[-1]
 
 print('Base folder path: ', base_folder_name)
-filepath = base_folder_name + sample_name + '/'
 print('Filepath: ', filepath)
 
 px_edge_rescaled = px_edge * rescale_factor
